2965113c35c3d16c32ab9990fe0c3e5df90e02ac
Diego Rios <diegoguitar90@gmail.com>
2020-05-29 12:53:37 -0300

    Creacion de Proyecto TP5 Programacion Visual 2020

---
+++ Rios_2700_TP5/src/main/java/ar/edu/unju/fi/Rios2700Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rios2700Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rios2700Tp5Application.class, args);
+	}
+
+}
